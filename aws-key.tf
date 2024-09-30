4a54acc82c1c29b259dbb95278d1d0aaf9f3aa4b
Alyssa Hoeben <alyssa.hoeben@viasat.com>
2024-09-30 10:29:06 -0700

    testsse

--- aws-key.tf
+++ aws-key.tf
@@ -9,12 +9,12 @@ terraform {
   required_version = ">= 0.13.5"
 }
 
-provider "aws" {
-  # profile    = var.profile
-  # region     = var.region
-  # access_key = var.access_key
-  # secret_key = var.secret_key
-}
+# provider "aws" {
+#   profile    = var.profile
+   region = "eu-west-1"
+#   access_key = var.access_key
+#   secret_key = var.secret_key
+# }
 
 resource "aws_key_pair" "deployer" {
   key_name   = var.key_name
